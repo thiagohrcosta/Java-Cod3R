c11442c7a00428c75203c1b8ffd46de90518b6aa
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-05-02 15:00:13 -0300

    Usando HashSet 2

---
+++ src/main/java/br/set/ConjuntoOrdenado.java
@@ -0,0 +1,13 @@
+package br.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoOrdenado {
+
+    public static void main(String[] args) {
+
+        Set<String> list = new HashSet();
+
+    }
+}
